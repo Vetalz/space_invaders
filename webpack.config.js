970bbff19d4ba55dec468ea3c36873056985a7c7
vetalz <vitaly482@gmail.com>
2022-04-13 16:59:16 +0300

    initial project

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    port: 3000
+  },
+  plugins: [
+    new HTMLWebpackPlugin({template: './src/index.html'}),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
